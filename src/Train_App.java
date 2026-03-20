6ca652829975946fe00ccf95f074e92b41c6a50c
santosh_1107 <saisantoshpreetham@gmail.com>
2026-03-20 15:17:14 +0530

    dev

--- src/Train_App.java
+++ src/Train_App.java
@@ -1,41 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Train_App {
 
-    public static void main(String[] args) {
 
-        System.out.println("=======================================");
-        System.out.println("UC2 - Add Passenger Bogies to Train");
-        System.out.println("=======================================\n");
-
-        // Create ArrayList for passenger bogies
-        List<String> passengerBogies = new ArrayList<>();
-
-        // Adding bogies
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
-
-        // Display after adding
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogies : " + passengerBogies + "\n");
-
-        // Remove a bogie
-        passengerBogies.remove("AC Chair");
-
-        // Display after removing
-        System.out.println("After Removing 'AC Chair':");
-        System.out.println("Passenger Bogies : " + passengerBogies + "\n");
-
-        // Check if Sleeper exists
-        System.out.println("Checking if 'Sleeper' exists:");
-        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper") + "\n");
-
-        // Final list
-        System.out.println("Final Train Passenger Consist:");
-        System.out.println(passengerBogies + "\n");
-
-        System.out.println("UC2 operations completed successfully...");
-    }
 }
